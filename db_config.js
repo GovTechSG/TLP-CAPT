210efec9de2755b24172d041952627d7d82e2288
Yu Ming <sohymg@users.noreply.github.com>
2021-09-01 11:53:13 +0800

    Create db_config.js

---
+++ db_config.js
@@ -0,0 +1,8 @@
+const knex = require('knex');
+const knexfile = require('knexfile');
+
+const env = process.env.NODE_EV || 'production';
+const configOptions = knexfile[env];
+
+
+module.exports = knex(configOptions);
